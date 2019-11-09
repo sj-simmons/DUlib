918f1fc3c3e36b4d73728c1ff5099415e10795cf
Scott Simmons <ssimmons1331@gmail.com>
2019-11-09 12:43:36 -0600

    set version to 0.3dev

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   author_email='ssimmons@drury.edu',
   packages=['du'],
   #install_requires=['torch'],
-  version='0.2',
+  version='0.3dev',
   license='Apache 2.0',
   description='DL tools courtesy of the DL@DU project',
   long_description=long_description
