a40fe9f039911226816e920bd67d9732cde7270f
Scott Simmons <ssimmons1331@gmail.com>
2020-12-03 21:32:06 -0600

    release 0.9.3

--- du/examples.py
+++ du/examples.py
@@ -517,9 +517,9 @@ import du.utils
 import du.models
 
 __author__ = 'Scott Simmons'
-__version__ = '0.9.2'
+__version__ = '0.9.3'
 __status__ = 'Development'
-__date__ = '11/08/20'
+__date__ = '12/03/20'
 __copyright__ = """
   Copyright 2019-2020 Scott Simmons
 
